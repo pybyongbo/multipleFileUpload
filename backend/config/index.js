2e9bd1fc958bf9b268453bcd7ce65d169797bd5e
pybyongbo-mac2024 <pyb901214@163.com>
2025-08-28 21:13:39 +0800

    test commit

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,22 +1,19 @@
 const config = {
-
   // 数据库配置
   database: {
-    DATABASE: "studentlist",
-    //  DATABASE: "nodejstest",
-    USERNAME: "root",
-    PASSWORD: "123456",
-    // PASSWORD: "root",
-    PORT: "3306",
-    HOST: "localhost",
+    // DATABASE: "studentlist",
+    DATABASE: 'nodejstest',
+    USERNAME: 'root',
+    // PASSWORD: "123456",
+    PASSWORD: 'root',
+    PORT: '3306',
+    HOST: 'localhost',
   },
 
   jwt: {
     secret: 'vue3-koa2-multiple-fileupload', // 实际项目中应该使用更复杂的密钥
-    expiresIn: '30m' // token 过期时间
-  }
+    expiresIn: '30m', // token 过期时间
+  },
 };
-
-
 
 module.exports = config;
