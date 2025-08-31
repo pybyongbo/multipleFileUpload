5157142bdc179d0442e386a6979eb58674a33085
pybyongbo-mac2024 <pyb901214@163.com>
2025-08-31 21:42:26 +0800

    环境变量区分,部署准备

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -7,9 +7,16 @@ import useUserStore from '@/store/modules/user';
 const isPro = import.meta.env.NODE_ENV === 'production';
 
 
-const devBaseUrl = 'http://localhost:3004'; // 确保端口是 3004
-const proBaseUrl = import.meta.env.API_DOMAIN;
-const baseURL = isPro ? proBaseUrl : devBaseUrl;
+// const devBaseUrl = 'http://localhost:3004'; // 确保端口是 3004
+// const proBaseUrl = import.meta.env.API_DOMAIN;
+// const baseURL = isPro ? proBaseUrl : devBaseUrl;
+// 使用 Vite 环境变量
+const baseURL = import.meta.env.VITE_API_BASE_URL || (import.meta.env.DEV ? 'http://localhost:3004' : '');
+
+console.log('API Base URL:', baseURL);
+console.log('Environment:', import.meta.env.MODE);
+
+console.log('baseURL',baseURL);
 
 const config = {
   timeout: 60000, // 超时时间
